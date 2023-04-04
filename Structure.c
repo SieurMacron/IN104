2d932b839c09dc350437656bc5fa1a6a78638e4d
SieurMacron <solal.marzouk@ensta-paris.fr>
2023-04-04 16:55:47 +0200

    Structure du code dans structure.c

---
+++ Structure.c
@@ -0,0 +1,11 @@
+//decomposition du jeu ///
+
+--Partie graphique
+
+--Partie Client (GUI)
+
+--PArtie serveur ( coeur du jeu)
+
+--Coeur du jeu
+Carte : matrice 
+Joueur =structure (puissance des bombes, pv, etc)
