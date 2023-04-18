f7a8d87cd59eaf94e2ecc873f94ff83cca45faa4
xxxClementhackingxxx <c.nedonsel@gmail.com>
2023-04-18 15:18:21 +0200

    j'ai tout modifié, merci louis maurin

--- Fenetre_jeu.c
+++ Fenetre_jeu.c
@@ -2,7 +2,7 @@
 pwindow= SDL_CreateWindow('Fenetre_jeu', SDL_WINDOWPOS_CENTERED /*coordonnées centrées*/, SDL_WINDOWPOS_CENTERED, 640/*largeur*/, 680/*hauteur*/, SDL_WINDOW_SHOWN, SDL_WINDOW_FULLSCREEN);
 if(NULL == window)
     {
-        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+        fprintf(stderr, "Erreur lors de la création de la fenetre : %s", SDL_GetError());
         return EXIT_FAILURE;
     }
 prenderer=SDL_CreateRenderer(pwindow, -1, SDL_RENDERER_ACCELERATED);
--- main.c
+++ main.c
@@ -19,6 +19,16 @@ int main(int argc, char *argv[])
 /*boucle principale*/
     SDL_Event events;  
     bool isOpen{ true };
+    /*Fenetre de jeu*/
+        pwindow= SDL_CreateWindow('Fenetre_jeu', SDL_WINDOWPOS_CENTERED /*coordonnées centrées*/, SDL_WINDOWPOS_CENTERED, 640/*largeur*/, 680/*hauteur*/, SDL_WINDOW_SHOWN, SDL_WINDOW_FULLSCREEN);
+        if(NULL == window)
+        {
+            fprintf(stderr, "Erreur lors de la création de la fenetre : %s", SDL_GetError());
+            return EXIT_FAILURE;
+        }
+        prenderer=SDL_CreateRenderer(pwindow, -1, SDL_RENDERER_ACCELERATED);
+        SDL_DestroyWindow(pWindow);  
+        SDL_DestroyRenderer(prenderer);
     while (isOpen){ /*tant que isOpen reste à true le jeu reste ouvert*/
         while (SDL_PollEvent(&events)){/*on code ici les conditions sous lesquelles le jeu se ferme*/
             switch (events.type){
